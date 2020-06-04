aafac5d30ec33c468c433065720deddf54827790
hissssst <csqcsqcsq@yandex.ru>
2020-06-04 17:11:28 +0300

    Bumped version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Pathex.MixProject do
   def project do
     [
       app: :pathex,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       description: description(),
